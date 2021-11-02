f69e56a2eea46330b6493541eb081c05546f49e7
Ranka Jovanovic <ranka.j.94@gmail.com>
2021-11-02 23:15:22 +0100

    updated router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
+
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import 'bootstrap';
 import router from './router'
-import 'mdb-vue-ui-kit/css/mdb.min.css';
+import 'mdb-ui-kit/css/mdb.min.css';
+
 import {store} from './store'
-
-
+import App from './App.vue'
 Vue.use(Vuex)
 Vue.use(VueRouter)
 
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import SingleItem from '../views/SingleItem.vue'
+import AddItem from '../views/AddItem.vue'
+import { globalAuthGuard } from '../guards/authGuard'
+
 
 Vue.use(VueRouter)
 
@@ -11,22 +14,31 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: Login,
+    meta: { guestRequired: true },
   },
   {
     path: '/register',
     name: 'register',
-    component: Register
+    component: Register,
+    meta: { guestRequired: true },
   },
   {
     path: '/items/:id',
     name: 'single-item',
-    component: SingleItem
+    component: SingleItem,
+    meta: { authRequired: true }
+  },
+  {
+    path: '/add',
+    name: 'add-item',
+    component: AddItem,
+    meta: { authRequired: true }
   },
 ]
 
@@ -35,5 +47,5 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-
+router.beforeEach(globalAuthGuard);
 export default router
